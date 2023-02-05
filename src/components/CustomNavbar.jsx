bab481487f48746cbb6c69999b602d2bca892b8b
Arth <arth4701@gmail.com>
2023-02-05 16:19:21 +0530

    current user display + loading svg

--- src/components/CustomNavbar.jsx
+++ src/components/CustomNavbar.jsx
@@ -20,10 +20,13 @@ export default function CustomNavbar() {
   const [isOpen, setIsOpen] = useState(false);
   let { user, logOut } = useUserAuth();
   const toggle = () => setIsOpen(!isOpen);
+
   return (
     <div className="my-nav">
       <Navbar expand="md" fixed="" className="px-3 shadow-sm">
-        <NavbarBrand tag={ReactLink} to="/home" className="logo">CAR.RENT </NavbarBrand>
+        <NavbarBrand tag={ReactLink} to="/home" className="logo">
+          CAR.RENT{" "}
+        </NavbarBrand>
         <NavbarToggler onClick={toggle} />
         <Collapse isOpen={isOpen} navbar>
           <Nav
@@ -35,54 +38,72 @@ export default function CustomNavbar() {
           ></Nav>
           <Nav>
             <NavItem>
-              <NavLink tag={ReactLink} to="/home" className="nav-items">Home </NavLink> 
+              <NavLink tag={ReactLink} to="/home" className="nav-items">
+                Home{" "}
+              </NavLink>
             </NavItem>
             <NavItem>
-              <NavLink tag={ReactLink} to="/about" className="nav-items">About</NavLink>
+              <NavLink tag={ReactLink} to="/about" className="nav-items">
+                About
+              </NavLink>
             </NavItem>
             <NavItem>
-              <NavLink tag={ReactLink} to="/dashboard"className="nav-items" >Dashboard</NavLink>
+              <NavLink tag={ReactLink} to="/dashboard" className="nav-items">
+                Dashboard
+              </NavLink>
             </NavItem>
             <NavItem>
-              <NavLink tag={ReactLink} to="/list-vehicle" className="nav-items">List Vehicle</NavLink>
+              <NavLink tag={ReactLink} to="/list-vehicle" className="nav-items">
+                List Vehicle
+              </NavLink>
             </NavItem>
-            {user &&
+            {user && (
+              <NavItem>
+                <NavLink tag={ReactLink} to="/home" className="nav-items">
+                  {user.displayName}
+                </NavLink>
+              </NavItem>
+            )}
+            {user && (
               <NavItem>
                 <NavLink>
                   <Button
                     style={{
                       background: "#5D5A88",
-                    }} onClick={logOut}
+                    }}
+                    onClick={logOut}
                   >
                     Logout
                   </Button>
-
                 </NavLink>
               </NavItem>
-            }
-            {!user && <NavItem>
-              <NavLink tag={ReactLink} to="/login" className="nav-logins">
-                <Button
-                  style={{
-                    background: "#5D5A88",
-                  }}
-                >
-                  Login
-                </Button>
-              </NavLink>
-            </NavItem>}
-            {!user && <NavItem>
-              <NavLink tag={ReactLink} to="/signUp" className="nav-logins">
-                <Button
-                  style={{
-                    background: "#5D5A88",
-                  }}
-                >
-                  Signup
-                </Button>
-              </NavLink>
-            </NavItem>}
-
+            )}
+            {!user && (
+              <NavItem>
+                <NavLink tag={ReactLink} to="/login" className="nav-logins">
+                  <Button
+                    style={{
+                      background: "#5D5A88",
+                    }}
+                  >
+                    Login
+                  </Button>
+                </NavLink>
+              </NavItem>
+            )}
+            {!user && (
+              <NavItem>
+                <NavLink tag={ReactLink} to="/signUp" className="nav-logins">
+                  <Button
+                    style={{
+                      background: "#5D5A88",
+                    }}
+                  >
+                    Signup
+                  </Button>
+                </NavLink>
+              </NavItem>
+            )}
           </Nav>
         </Collapse>
       </Navbar>
